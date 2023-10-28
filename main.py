cfe8a052e315eafa979b5f0639b6f5d73e7d260b
Parham Askarzadeh <p.askarzadeh@lifeweb.ir>
2023-10-28 17:39:07 +0330

    create main structure of cod & add necessary function

--- main.py
+++ main.py
@@ -3,3 +3,31 @@ import simpy
 import operator
 import matplotlib.pyplot as plt
 
+
+class Scheduler(object):
+    def __init__(self):
+        pass
+
+    def job_creator(self):
+        pass
+
+    def run(self):
+        pass
+
+    def check_timeout(self):
+        pass
+
+    def dispatcher(self):
+        pass
+
+    def job_loader(self):
+        pass
+
+    def round_robin_t2_process(self):
+        pass
+
+    def first_come_first_serve_process(self):
+        pass
+
+    def analyse(self):
+        pass
